1a6695ee66df9005bd73bd745e9d35a3d404ac3a
VankataLv <132002763+VankataLv@users.noreply.github.com>
2023-06-03 10:12:30 +0300

    1.1

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ total_games_played = 0
 board = [el for el in range(10)]
 valid_choices = [str(el) for el in range(1, 10)]
 win_lose_draw = False
-user_desire_to_play = True
+user_choice_to_play = True
 print("______________________________________________")
 print("> Dear player, welcome to my Tic-Tac_Toe game!")
 while True:
@@ -18,7 +18,7 @@ while True:
         break
 
 print(f"> Hello {user_name}!\n> Let's play a game!, you will be with X symbol")
-while user_desire_to_play:
+while user_choice_to_play:
     while not win_lose_draw:
         functions.current_board(board)
         print(f'> {user_name}, please make your move! Pick a number from 1 to 9 and then press "enter"!')
@@ -35,12 +35,16 @@ while user_desire_to_play:
 
     print('> Do you want to play again? Type "y" or "n".')
     user_choice_to_play = input("> ").lower()
-    if user_choice_to_play == "n":
-        print('> Thank you for playing!')
-        user_desire_to_play = False
-    elif user_choice_to_play == "y":
-        print("> Excellent, let's play again!")
-    else:
-        print('> Please try again! Do you want to play another game? Type "y" or "n".')
+    while True:
+        if user_choice_to_play == "n":
+            print('> Thank you for playing!')
+            user_desire_to_play = False
+            break
+        elif user_choice_to_play == "y":
+            print("> Excellent, let's play again!")
+            break
+        else:
+            print('> Please try again! Do you want to play another game? Type "y" or "n".')
+            continue
 
 print(f"Goodbye {user_name}!")
